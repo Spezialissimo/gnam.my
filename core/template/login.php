b2a2a8f3638344fb79e9c82f3ae4150040c9a66b
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 15:17:31 +0100

    Add grid to profile

--- core/template/login.php
+++ core/template/login.php
@@ -5,7 +5,7 @@
             <h1 class="fw-bold">Accedi</h1>
             <input type="text" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Username">
             <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-4" placeholder="Password">
-            <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50 mt-3">Login</button>
+            <a href="home.php" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50 mt-3">Login</a>
             <hr class="mt-3">
             <p class="h6 fw-bold">Non hai un account?</p>
             <a href="register.php" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Registrati</a>
---
+++ core/template/profile.php
@@ -0,0 +1,53 @@
+<div class="container text-center">
+  <div class="row">
+    <div class="col-sm-4">
+        <img class="border border-4 border-dark rounded-circle w-25" alt="Filippo Champagne" src="assets/prova-profile.png" />
+    </div>
+    <div class="col-sm-8">
+        <div class="row">
+            <h2>GiorgioneErRomano</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 mb-0">
+                <p class="fw-bold">Follower</p>
+                <p class="mt-0">0</p>
+            </div>
+            <div class="col-sm-4 mb-0">
+                <p class="fw-bold">Seguiti</p>
+                <p class="mt-0">0</p>
+            </div>
+            <div class="col-sm-4 mb-0">
+                <p class="fw-bold">Gnam</p>
+                <p class="mt-0">0</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 mb-2">
+        <button type="button" class="btn btn-bounce rounded-pill bg-primary fw-bold text-white">Segui</button>
+        <button type="button" class="btn btn-bounce rounded-pill bg-primary fw-bold text-white">Condividi</button>
+    </div>
+  </div>
+</div>
+
+<div class="container overflow-y-scroll" style="max-height: 15%;">
+    <div class="row">
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+    </div>
+    <div class="row mt-3 mb-3">
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+    </div>
+    <div class="row mt-3 mb-3">
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+    </div>
+    <div class="row mt-3 mb-3">
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+        <img class="img-grid col" alt="Filippo Champagne" src="assets/prova.png" />
+    </div>
+</div>
