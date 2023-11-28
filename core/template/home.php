23a8fbfb3910f83c9273cc60a9665964067083e2
Mauro Pellonara <mauro.pellonara@gmail.com>
2023-11-28 16:50:05 +0100

    Push home

--- core/template/home.php
+++ core/template/home.php
@@ -1,7 +1,66 @@
 <div class="position-relative w-100 p-0">
     <video class="w-100 p-0" autoplay disablepictureinpicture loop playsinline preload="auto" poster="assets/prova.png" src="assets/prova.mp4"></video>
-    <div class="justify-content-center align-items-end top-0 left-0 w-100 d-flex position-absolute p-0 bg opacity-25" id="videoOverlay">
-        <p>This is an overlay</p>
+    <div class="video-overlay" id="videoOverlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-10 align-self-center">
+                    <div class="row">
+                        <div class="col-4">
+                            <img class="border border-4 border-dark rounded-circle w-100" alt="Filippo Champagne" src="assets/prova-profile.png" />
+                        </div>
+                        <div class="col-8 d-flex align-items-center">
+                            <p class="fs-5 fw-bold m-0">Profilo Nome</p>
+                        </div>
+                    </div>
+                    <div class="row m-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit, tortor ut gravida pellentesque, risus. Leggi di piú...</p>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <span class="badge rounded-pill bg-primary fw-light text-black">
+                	        	<i class="fa-solid fa-oil-can"></i>&nbsp #Untazzo
+                	        </span>
+                        </div>
+                        <div class="col-4">
+                	        <span class="badge rounded-pill bg-primary fw-light text-black">
+                		        <i class="fa-solid fa-leaf"></i>&nbsp #Vegan
+                	        </span>
+                        </div>
+                        <div class="col-2 ps-0">
+                	        <span class="badge rounded-pill bg-primary fw-light text-black">
+                                <i class="fa-solid fa-ellipsis"></i>
+                	        </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col">
+                                <span>
+                                    <i class="fa-solid fa-heart fa-2xl fa-fw"></i>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="row pt-4">
+                            <div class="col">
+                                <i class="fa-solid fa-utensils fa-2xl fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="row pt-4">
+                            <div class="col">
+                                <i class="fa-solid fa-comment-dots fa-2xl fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="row pt-4">
+                            <div class="col">
+                                <i class="fa-solid fa-share-nodes fa-2xl fa-fw"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
