1813bff9a7bcdea822cb3200f01e6a421181f334
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 10:27:43 +0100

    Add some template files

---
+++ cerca.php
@@ -0,0 +1,5 @@
+<?php require_once('core/template/header.php'); ?>
+
+<?php require_once('core/template/cerca.php'); ?>
+
+<?php require_once('core/template/footer.php'); ?>
---
+++ core/template/header.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+if (isset($_SESSION['user_id']) == false) {
+    header('Location: index.php');
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="css/style.css" />
+	<script src="js/main.js"></script>
+
+	<script src="https://kit.fontawesome.com/1df86e7f33.js" crossorigin="anonymous"></script>
+
+	<title><?php echo $pageTitle ?></title>
+</head>
+<body>
