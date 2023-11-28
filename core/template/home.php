f25413776bb71fd25bc628a913e07dd6d8dc36f3
Mauro Pellonara <mauro.pellonara@outlook.com>
2023-11-28 23:42:35 +0100

    complete home initial layout

--- core/template/home.php
+++ core/template/home.php
@@ -1,19 +1,19 @@
 <div class="position-relative w-100 p-0">
     <video class="w-100 p-0" autoplay disablepictureinpicture loop playsinline preload="auto" poster="assets/prova.png" src="assets/prova.mp4"></video>
     <div class="video-overlay" id="videoOverlay">
-        <div class="container">
+        <div class="container mb-3">
             <div class="row">
-                <div class="col-10 align-self-center">
+                <div class="col-10">
                     <div class="row">
                         <div class="col-4">
                             <img class="border border-4 border-dark rounded-circle w-100" alt="Filippo Champagne" src="assets/prova-profile.png" />
                         </div>
-                        <div class="col-8 d-flex align-items-center">
+                        <div class="col-8 d-flex align-items-center p-0">
                             <p class="fs-5 fw-bold m-0">Profilo Nome</p>
                         </div>
                     </div>
-                    <div class="row m-0">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit, tortor ut gravida pellentesque, risus. Leggi di piú...</p>
+                    <div class="row">
+                        <p class="m-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit, tortor ut gravida pellentesque, risus. Leggi di piú...</p>
                     </div>
                     <div class="row">
                         <div class="col-4">
@@ -35,26 +35,18 @@
                 </div>
                 <div class="col-2">
                     <div class="container p-0">
-                        <div class="row">
-                            <div class="col">
-                                <span>
-                                    <i class="fa-solid fa-heart fa-2xl fa-fw"></i>
-                                </span>
+                        <div class="col">
+                            <div class="row pt-4">
+                                <span><i class="fa-solid fa-heart fa-2xl fa-fw color-secondary"></i></span>
                             </div>
-                        </div>
-                        <div class="row pt-4">
-                            <div class="col">
-                                <i class="fa-solid fa-utensils fa-2xl fa-fw"></i>
+                            <div class="row pt-4">
+                                <span><i class="fa-solid fa-utensils fa-2xl fa-fw color-secondary"></i></span>
                             </div>
-                        </div>
-                        <div class="row pt-4">
-                            <div class="col">
-                                <i class="fa-solid fa-comment-dots fa-2xl fa-fw"></i>
+                            <div class="row pt-4">
+                                <span><i class="fa-solid fa-comment-dots fa-2xl fa-fw color-secondary"></i></span>
                             </div>
-                        </div>
-                        <div class="row pt-4">
-                            <div class="col">
-                                <i class="fa-solid fa-share-nodes fa-2xl fa-fw"></i>
+                            <div class="row pt-4">
+                                <span><i class="fa-solid fa-share-nodes fa-2xl fa-fw color-secondary"></i></span>
                             </div>
                         </div>
                     </div>
