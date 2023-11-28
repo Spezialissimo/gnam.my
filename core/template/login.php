eb1a9f3664cea7c23abc131b3641eaca405b3d3d
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 13:13:17 +0100

    Beautify login CSS

--- core/template/login.php
+++ core/template/login.php
@@ -1,18 +1,12 @@
 <div class="container text-center">
-    <div class="row">
-        <div class="col"></div>
-        <div class="col">
+    <div class="row align-items-center">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
             <h1 class="fw-bold">Accedi</h1>
-            <div class="mb-3">
-                <input type="text" class="form-control bg-primary rounded shadow-sm" placeholder="Username">
-            </div>
-            <div class="mb-3">
-                <input type="password" class="form-control bg-primary rounded shadow-sm" placeholder="Password">
-            </div>
-            <div class="d-grid gap-2 col-6 mx-auto">
-                <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Login</button>
-            </div>
+            <input type="text" class="form-control bg-primary rounded shadow-sm mb-3" placeholder="Username">
+            <input type="password" class="form-control bg-primary rounded shadow-sm mb-3" placeholder="Password">
+            <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50">Login</button>
         </div>
-        <div class="col"></div>
+        <div class="col-md-4"></div>
     </div>
 </div>
