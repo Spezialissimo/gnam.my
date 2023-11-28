0f9ad59bc2ad719e19c1322743ce0b1d674c39b0
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 13:50:44 +0100

    Add register page

---
+++ core/template/register.php
@@ -0,0 +1,16 @@
+<div class="container text-center">
+    <div class="row align-items-center">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <h1 class="fw-bold">Registrati</h1>
+            <input type="text" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Username">
+            <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-4" placeholder="Password">
+            <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-4" placeholder="Conferma Password">
+            <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50 mt-3">Registrati</button>
+            <hr class="mt-3">
+            <p class="h6 fw-bold">Hai già un account?</p>
+            <a href="login.php" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Accedi</a>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
