35722ceb23a24941b22476b228f2511ed0deca2c
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 11:19:45 +0100

    Fix login page footer

--- core/template/footer.php
+++ core/template/footer.php
@@ -1,13 +1,15 @@
     </main>
-    <div class="bg container fixed-bottom w-100 m-0">
-		<div class="row gx-3">
-			<!-- Tutto su una riga per facilità di modifica, da aggiustare TODO -->
-			<!-- Home button         --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-house  fa-lg fa-fw"></i></button>
-			<!-- Seaerch button      --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-search fa-lg fa-fw"></i></button>
-			<!-- Add button          --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-plus   fa-lg fa-fw"></i></button>
-			<!-- Notification button --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-bell   fa-lg fa-fw"></i></button>
-			<!-- Profile button      --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-user   fa-lg fa-fw"></i></button>
+	<?php if(PAGE_TITLE != 'Login' && PAGE_TITLE != 'Registrati') { ?>
+		<div class="bg container fixed-bottom w-100 m-0">
+			<div class="row gx-3">
+				<!-- Tutto su una riga per facilità di modifica, da aggiustare TODO -->
+				<!-- Home button         --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-house  fa-lg fa-fw"></i></button>
+				<!-- Seaerch button      --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-search fa-lg fa-fw"></i></button>
+				<!-- Add button          --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-plus   fa-lg fa-fw"></i></button>
+				<!-- Notification button --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-bell   fa-lg fa-fw"></i></button>
+				<!-- Profile button      --><button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 p-2 pb-3 pt-3"><i class="fa-solid fa-user   fa-lg fa-fw"></i></button>
+			</div>
 		</div>
-	</div>
+	<?php } ?>
 </body>
 </html>
