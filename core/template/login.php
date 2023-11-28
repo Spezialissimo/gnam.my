359e98772b5a6c0796ca595fbee2448e5716d96c
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 11:33:57 +0100

    Add basic login + fix input elements border

---
+++ core/template/login.php
@@ -0,0 +1,12 @@
+<div class="container text-center">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col">
+            <h1 class="fw-bold">Accedi</h1>
+            <div class="mb-3">
+                <input type="text" class="form-control bg-primary rounded shadow-sm" placeholder="Username">
+            </div>
+        </div>
+        <div class="col"></div>
+    </div>
+</div>
