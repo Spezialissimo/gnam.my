b36590ad8d7993d2094e5483389a044c4dc72685
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-28 15:49:01 +0100

    Fix register

--- core/template/register.php
+++ core/template/register.php
@@ -1,16 +1,10 @@
-<div class="container text-center">
-    <div class="row align-items-center">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <h1 class="fw-bold">Registrati</h1>
-            <input type="text" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Username">
-            <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-4" placeholder="Password">
-            <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-4" placeholder="Conferma Password">
-            <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50 mt-3">Registrati</button>
-            <hr class="mt-3">
-            <p class="h6 fw-bold">Hai già un account?</p>
-            <a href="login.php" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Accedi</a>
-        </div>
-        <div class="col-md-4"></div>
-    </div>
+<div class="d-flex flex-column align-items-center p-4 h-100 justify-content-center text-center">
+    <h1 class="fw-bold">Registrati</h1>
+    <input type="text" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Username">
+    <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Password">
+    <input type="password" class="form-control bg-primary rounded shadow-sm mb-3 mt-3" placeholder="Conferma Password">
+    <button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-50 mt-3">Registrati</button>
+    <hr class="mt-3">
+    <p class="h6 fw-bold">Hai già un account?</p>
+    <a href="login.php" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Accedi</a>
 </div>
