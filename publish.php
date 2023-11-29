3e552f697b8c94423e50824bae09a84a2f2395f3
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-29 10:09:41 +0100

    Fix last navbar link

--- publish.php
+++ publish.php
@@ -3,7 +3,7 @@
 define('PAGE_TITLE', 'Pubblica Gnam');
 
 require_once('core/template/header.php');
-require_once('core/template/share.php');
+require_once('core/template/publish.php');
 require_once('core/template/footer.php');
 
 ?>
