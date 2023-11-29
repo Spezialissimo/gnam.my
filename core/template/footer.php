2b8cc56671dd2e0c4700c8d2a95d099315602689
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-29 10:03:25 +0100

    Add navbar links

--- core/template/footer.php
+++ core/template/footer.php
@@ -1,11 +1,11 @@
 <?php if(PAGE_TITLE != 'Login' && PAGE_TITLE != 'Registrati') { ?>
 			</div>
 			<div class="row m-0 p-2 ps-4 pe-4" id="navbarDiv">
-				<button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-house fa-lg fa-fw"></i></button>
-				<button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-search fa-lg fa-fw"></i></button>
-				<button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-plus fa-lg fa-fw"></i></button>
-				<button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-bell fa-lg fa-fw"></i></button>
-				<button type="button" class="col btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-user fa-lg fa-fw"></i></button>
+				<a role="button" href="home.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-house fa-lg fa-fw"></i></a>
+				<a role="button" href="search.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-search fa-lg fa-fw"></i></a>
+				<a role="button" href="share.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-plus fa-lg fa-fw"></i></a>
+				<a role="button" href="notifications.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-bell fa-lg fa-fw"></i></a>
+				<a role="button" href="profile.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 mt-0 mb-0 p-1 pt-2 pb-2"><i class="fa-solid fa-user fa-lg fa-fw"></i></a>
 			</div>
 		</div>
 <?php } ?>
