434964f31471ae97db5757f748f23eb93f1897ee
Spezialissimo <davide.speziali@proton.me>
2023-11-29 11:35:06 +0100

    Add publish

--- core/template/publish.php
+++ core/template/publish.php
@@ -1,6 +1,43 @@
-<!-- File upload -->
-<input type="file" class="form-control bg-primary rounded shadow-sm" />
-<input type="file" class="form-control bg-primary rounded shadow-sm" />
+<div class="container text-center font-text">
+    <div class="row-2 py-2 h4" id="headerDiv">
+        <h1 class="fw-bold">Pubblica Gnam</h1>
+    </div>
 
-<!-- Textarea -->
-<textarea class="form-control bg-primary rounded shadow-sm" rows="3"></textarea>
+    <!-- video chooser field -->
+    <div class="row container overflow-auto p-0 m-0" id="pageContentDiv">
+        <div class="col">
+            <div class="row-md px-4 h4">
+                <h2 class="fw-bold">Scegli video</h2>
+                <input type="file" class="form-control bg-primary rounded shadow-sm" />
+            </div>
+            <!-- thumbnail chooser field -->
+            <div class="row-md px-4 h4">
+                <h2 class="fw-bold">Scegli copertina</h2>
+                <input type="file" class="form-control bg-primary rounded shadow-sm" />
+            </div>
+            <!-- description field -->
+            <div class="row-md-6 px-4 h4">
+                <h2 class="fw-bold">Descrizione</h2>
+                <textarea class="form-control bg-primary rounded shadow-sm" rows="3"></textarea>
+            </div>
+            <!-- ingredients -->
+            <div class="row-sm pt-2 pb-0 ">
+                <!-- Button con counter -->
+                <button type="button" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white">
+                    Ingredienti <span class="badge rounded-pill bg-accent">1</span>
+                </button>
+            </div>
+            <!-- tag -->
+            <div class="row-sm pt-1 h-0">
+                <!-- Button con counter -->
+                <button type="button" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white">
+                    Tag <span class="badge rounded-pill bg-accent">1</span>
+                </button>
+            </div>
+            <!-- read all notification button -->
+            <div class="row-md-4 pt-4">
+                <a href="#" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Pubblica Gnam</a>
+            </div>
+        </div>
+    </div>
+</div>
