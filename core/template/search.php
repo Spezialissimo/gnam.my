6e5475b58b819914a70b4c69433c2636d008312a
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-30 10:18:37 +0100

    Add complete ingredients hangling for Search page

--- core/template/search.php
+++ core/template/search.php
@@ -13,7 +13,7 @@
     <div class="row-md-2 py-2">
         <!-- Button con counter -->
         <button type="button" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white" id="ingredientsButton">
-            Ingredienti <span class="badge rounded-pill bg-accent">1</span>
+            Ingredienti <span class="badge rounded-pill bg-accent" id="ingredientsCount">0</span>
         </button>
     </div>
 </div>
@@ -34,19 +34,65 @@
     let ingredients = [];
 
     const openIngredients = () => {
-        let html = '<div class="row-md-2 py-2"><div class="input-group rounded"><span class="input-group-text bg-primary border-0"><i class="fa-solid fa-magnifying-glass"></i></span><input type="text" class="form-control bg-primary shadow-sm" placeholder="Cerca Ingredienti"></div></div><hr><div class="text-center" id="searchedIngredients"></div><hr><div class="row m-0 p-0"><div class="col-6"><button type="button" class="btn btn-bounce rounded-pill bg-alert fw-bold text-white w-100" id="reset">Reset</button></div><div class="col-6"><button type="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-100" id="addElement">Ok</button></div></div>';
-        showSwal('Scegli ingredienti', html);
+        let modalContent = '';
+
+        if (ingredients.length > 0) {
+            modalContent = ingredients.map(ingredient => '<p class="fw-bold"><button type="button" class="btn btn-bounce bg-primary text-black" onclick="removeIngredient(this)"><i class="fa-solid fa-trash-can"></i></button>&nbsp' + ingredient + '</p>').join('');
+        }
+
+        let html = `<div class="row-md-2 py-2">
+                        <div class="input-group rounded">
+                            <span class="input-group-text bg-primary border-0"><i class="fa-solid fa-magnifying-glass"></i></span>
+                            <input type="text" id="ingredientInput" class="form-control bg-primary shadow-sm" placeholder="Cerca Ingredienti">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="text-center" id="searchedIngredients">${modalContent}</div>
+                    <hr>
+                    <div class="row m-0 p-0">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-bounce rounded-pill bg-alert fw-bold text-white w-100" onclick="resetIngredients()">Reset</button>
+                        </div>
+                        <div class="col-6">
+                            <button type="button" id="okButton" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-100">Ok</button>
+                        </div>
+                    </div>`;
+
+        const modal = showSwal('Scegli ingredienti', html);
+
+        $('#ingredientInput').keypress(function(event) {
+            if (event.which === 13) {
+                addIngredient();
+            }
+        });
+
+        $('#okButton').click(function() {
+            closeSwal();
+        });
     }
 
-    const addIngredient = (ingredient) => {
-        let newElement = "Banane";
-        ingredients.push(newElement);
-        $("#searchedIngredients").append('<p class="fw-bold"><button type="button" class="btn btn-bounce bg-primary text-black"><i class="fa-solid fa-trash-can"></i></button>&nbsp' + newElement + '</p>');
+    const addIngredient = () => {
+        let newIngredient = $('#ingredientInput').val().trim();
+        if (!newIngredient || ingredients.includes(newIngredient)) {
+            return;
+        }
+        ingredients.push(newIngredient);
+        $("#searchedIngredients").append('<p class="fw-bold"><button type="button" class="btn btn-bounce bg-primary text-black" onclick="removeIngredient(this)"><i class="fa-solid fa-trash-can"></i></button>&nbsp' + newIngredient + '</p>');
+        $('#ingredientInput').val('');
+        $('#ingredientsCount').html(ingredients.length);
+    }
+
+    const removeIngredient = (element) => {
+        let indexToRemove = $(element).parent().index();
+        ingredients.splice(indexToRemove, 1);
+        $(element).parent().remove();
+        $('#ingredientsCount').html(ingredients.length);
     }
 
     const resetIngredients = () => {
         ingredients = [];
         $("#searchedIngredients").empty();
+        $('#ingredientsCount').html(ingredients.length);
     }
 
     $("#ingredientsButton").on("click", openIngredients);
