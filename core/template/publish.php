733520c91cf3ea1c60fb6577412aa535001a19e6
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-11-30 10:46:01 +0100

    Add hashtag modal and handling in Publish page

--- core/template/publish.php
+++ core/template/publish.php
@@ -24,19 +24,19 @@
             <div class="row-sm pt-2 pb-0 ">
                 <!-- Button con counter -->
                 <button type="button" onclick="chooseIngredients();" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white">
-                    Ingredienti <span class="badge rounded-pill bg-accent">1</span>
+                    Ingredienti <span class="badge rounded-pill bg-accent">0</span>
                 </button>
             </div>
             <!-- tag -->
             <div class="row-sm pt-1 h-0">
                 <!-- Button con counter -->
-                <button type="button" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white">
-                    Tag <span class="badge rounded-pill bg-accent">1</span>
+                <button type="button" class="btn btn-bounce rounded-pill bg-secondary fw-bold text-white" id="hashtagsButton" >
+                    Hashtag <span class="badge rounded-pill bg-accent" id="hashtagsCount">0</span>
                 </button>
             </div>
             <!-- read all notification button -->
             <div class="row-md-4 pt-4">
-                <a href="#" role="button" onclick="publish();" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white">Pubblica Gnam</a>
+                <a href="#" role="button" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white" id="publishBtn">Pubblica Gnam</a>
             </div>
         </div>
     </div>
@@ -54,4 +54,76 @@
         let html = '<div class="row-md-2 py-2 text-center text-black"><i class="fa-solid fa-check fa-2xl"></i></div>';
         showSwalSmall('Gnam pubblicato', html);
     }
+
+    let hashtags = [];
+
+    const openHashtags = () => {
+        let modalContent = '';
+
+        if (hashtags.length > 0) {
+            modalContent = hashtags.map(hashtag => '<p class="fw-bold"><button type="button" class="btn btn-bounce bg-primary text-black" onclick="removeHashtag(this)"><i class="fa-solid fa-trash-can"></i></button>&nbsp#' + hashtag + '</p>').join('');
+        }
+
+        let html = `<div class="row-md-2 py-2">
+                        <div class="input-group rounded">
+                            <span class="input-group-text bg-primary border-0"><i class="fa-solid fa-magnifying-glass"></i></span>
+                            <input type="text" id="hashtagInput" class="form-control bg-primary shadow-sm" placeholder="Cerca Hashtag">
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="text-center" id="searchedHashtags">${modalContent}</div>
+                    <hr>
+                    <div class="row m-0 p-0">
+                        <div class="col-6">
+                            <button type="button" class="btn btn-bounce rounded-pill bg-alert fw-bold text-white w-100" onclick="resetHashtags()">Reset</button>
+                        </div>
+                        <div class="col-6">
+                            <button type="button" id="okButton" class="btn btn-bounce rounded-pill bg-accent fw-bold text-white w-100">Ok</button>
+                        </div>
+                    </div>`;
+
+        const modal = showSwal('Scegli hashtag', html);
+
+        $('#hashtagInput').keypress(function(event) {
+            if (event.which === 13) {
+                addHashtag();
+            }
+        });
+
+        $('#okButton').click(function() {
+            closeSwal();
+        });
+    }
+
+    const addHashtag = () => {
+        let newHashtag = $('#hashtagInput').val().trim();
+        while(newHashtag.startsWith('#')) {
+            newHashtag = newHashtag.slice(1);
+        }
+        newHashtag = '#' + newHashtag;
+        if (!newHashtag || hashtags.includes(newHashtag)) {
+            return;
+        }        
+        hashtags.push(newHashtag);
+        $("#searchedHashtags").append('<p class="fw-bold"><button type="button" class="btn btn-bounce bg-primary text-black" onclick="removeHashtag(this)"><i class="fa-solid fa-trash-can"></i></button>&nbsp' + newHashtag + '</p>');
+        $('#hashtagInput').val('');
+        $('#hashtagsCount').html(hashtags.length);
+    }
+
+
+    const removeHashtag = (element) => {
+        let indexToRemove = $(element).parent().index();
+        hashtags.splice(indexToRemove, 1);
+        $(element).parent().remove();
+        $('#hashtagsCount').html(hashtags.length);
+    }
+
+    const resetHashtags = () => {
+        hashtags = [];
+        $("#searchedHashtags").empty();
+        $('#hashtagsCount').html(hashtags.length);
+    }
+    
+    $("#publishBtn").on("click", publish);
+    $("#hashtagsButton").on("click", openHashtags);
 </script>
