48437e6c70ae836a8add8c250f218d5b7c952a67
Mauro Pellonara <mauro.pellonara@gmail.com>
2023-12-07 20:06:21 +0100

    fix videos upload

--- api/videos.php
+++ api/videos.php
@@ -4,7 +4,7 @@ header("Content-Type: multipart/form-data; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if (isset($_POST["video"]) && isset($_POST["api_key"])) {
+if (isset($_FILES["video"]) && isset($_POST["api_key"])) {
     if (!isset($_POST["thumbnail"])) {
         // TODO: crea thumbnail
     }
