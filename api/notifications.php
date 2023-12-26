eddf7e2a59bd2a5511864a4021c63946814a11f4
Mauro Pellonara <mauro.pellonara@gmail.com>
2023-12-27 00:00:59 +0100

    try fixing server not accepting delete requests

--- api/notifications.php
+++ api/notifications.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 require_once("../core/functions.php");
 
