6f91391c05b7a2b728d585e114eed99c434ecd3c
Mauro Pellonara <mauro.pellonara@outlook.com>
2024-01-09 15:12:44 +0100

    remove not needed classes and move btn-bounce to div

--- core/template/footer.php
+++ core/template/footer.php
@@ -2,32 +2,32 @@
 			</div>
 			<div class="m-0" id="navbarDiv">
 				<div class="w-100 d-flex" id="navbarContentDiv">
-					<div class="d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="home.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent d-flex p-3">
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="home.php" class="btn nav-btn bg-primary d-flex p-3">
 							<img alt="Pagina Home" src="assets/home.png" />
 						</a>
 						<a href="home.php" class="ps-2 text-button color-accent">Home</a>
 					</div>
-					<div class="d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="search.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent d-flex p-3">
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="search.php" class="btn nav-btn bg-primary d-flex p-3">
 							<img alt="Pagina Cerca" src="assets/search.png" />
 						</a>
 						<a href="search.php" class="ps-2 text-button color-accent">Cerca</a>
 					</div>
-					<div class="d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="publish.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent d-flex p-3">
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="publish.php" class="btn nav-btn bg-primary d-flex p-3">
 							<img alt="Pagina Pubblica" src="assets/publish.png" />
 						</a>
 						<a href="publish.php" class="ps-2 text-button color-accent">Pubblica</a>
 					</div>
-					<div class="d-flex flex-row align-items-center justify-content-center">
-						<a role="button" id="notificationsNavbarButton" href="notifications.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent d-flex p-3">
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
+						<a role="button" id="notificationsNavbarButton" href="notifications.php" class="btn nav-btn bg-primary d-flex p-3">
 							<img alt="Pagina Notifiche" src="assets/notifications.png" />
 						</a>
 						<a href="notifications.php" class="ps-2 text-button color-accent">Notifiche</a>
 					</div>
-					<div class="d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="profile.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent d-flex p-3">
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="profile.php" class="btn nav-btn bg-primary d-flex p-3">
 							<img alt="Pagina Profilo" src="assets/profile.png" />
 						</a>
 						<a href="profile.php" class="ps-2 text-button color-accent">Profilo</a>
