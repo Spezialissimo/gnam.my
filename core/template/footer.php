61b71a028d7f90214916390d664bd449cff8eaff
Mauro Pellonara <mauro.pellonara@outlook.com>
2024-01-09 12:03:00 +0100

    add navbar text

--- core/template/footer.php
+++ core/template/footer.php
@@ -3,11 +3,27 @@
 			</div>
 			<div class="m-0" id="navbarDiv">
 				<div class="w-100 d-flex" id="navbarContentDiv">
-					<a role="button" href="home.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-house fa-lg fa-fw"></i></a>
-					<a role="button" href="search.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-search fa-lg fa-fw"></i></a>
-					<a role="button" href="publish.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-plus fa-lg fa-fw"></i></a>
-					<a role="button" id="notificationsNavbarButton" href="notifications.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 my-0 p-1 py-2"><i class="fa-solid fa-bell fa-lg fa-fw"></i></a>
-					<a role="button" href="profile.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-user fa-lg fa-fw"></i></a>
+					<div class="d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="home.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-house fa-lg fa-fw"></i></a>
+						<a href="home.php" class="ps-2 text-button color-accent">Home</a>
+					</div>
+					<div class="d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="search.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent">
+							<i class="fa-solid fa-search fa-lg fa-fw"></i></a>
+						<a href="search.php" class="ps-2 text-button color-accent">Cerca</a>
+					</div>
+					<div class="d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="publish.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-plus fa-lg fa-fw"></i></a>
+						<a href="publish.php" class="ps-2 text-button color-accent">Pubblica</a>
+					</div>
+					<div class="d-flex flex-row align-items-center justify-content-center">
+						<a role="button" id="notificationsNavbarButton" href="notifications.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent m-2 my-0 p-1 py-2"><i class="fa-solid fa-bell fa-lg fa-fw"></i></a>
+						<a href="notifications.php" class="ps-2 text-button color-accent">Notifiche</a>
+					</div>
+					<div class="d-flex flex-row align-items-center justify-content-center">
+						<a role="button" href="profile.php" class="col btn nav-btn btn-lg btn-bounce bg-primary color-accent"><i class="fa-solid fa-user fa-lg fa-fw"></i></a>
+						<a href="profile.php" class="ps-2 text-button color-accent">Profilo</a>
+					</div>
 				</div>
 			</div>
 		</div>
