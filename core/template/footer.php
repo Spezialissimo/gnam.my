81ab678f1021125b087716474e910840e1cf55f9
Mauro Pellonara <mauro.pellonara@gmail.com>
2024-01-12 12:14:21 +0100

    add semibold to navbar text

--- core/template/footer.php
+++ core/template/footer.php
@@ -6,31 +6,31 @@
 						<a role="button" href="home.php" class="btn nav-btn bg-primary d-flex p-sm-3 p-2">
 							<img alt="Pagina Home" src="assets/home.png" />
 						</a>
-						<a href="home.php" class="ps-2 text-button color-accent">Home</a>
+						<a href="home.php" class="ps-2 text-button fw-semibold color-accent">Home</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
 						<a role="button" href="search.php" class="btn nav-btn bg-primary d-flex p-sm-3 p-2">
 							<img alt="Pagina Cerca" src="assets/search.png" />
 						</a>
-						<a href="search.php" class="ps-2 text-button color-accent">Cerca</a>
+						<a href="search.php" class="ps-2 text-button fw-semibold color-accent">Cerca</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
 						<a role="button" href="publish.php" class="btn nav-btn bg-primary d-flex p-sm-3 p-2">
 							<img alt="Pagina Pubblica" src="assets/publish.png" />
 						</a>
-						<a href="publish.php" class="ps-2 text-button color-accent">Pubblica</a>
+						<a href="publish.php" class="ps-2 text-button fw-semibold color-accent">Pubblica</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
 						<a role="button" id="notificationsNavbarButton" href="notifications.php" class="btn nav-btn bg-primary d-flex p-sm-3 p-2">
 							<img alt="Pagina Notifiche" src="assets/notifications.png" />
 						</a>
-						<a href="notifications.php" class="ps-2 text-button color-accent">Notifiche</a>
+						<a href="notifications.php" class="ps-2 text-button fw-semibold color-accent">Notifiche</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
 						<a role="button" href="profile.php" class="btn nav-btn bg-primary d-flex p-sm-3 p-2">
 							<img alt="Pagina Profilo" src="assets/profile.png" />
 						</a>
-						<a href="profile.php" class="ps-2 text-button color-accent">Profilo</a>
+						<a href="profile.php" class="ps-2 text-button fw-semibold color-accent">Profilo</a>
 					</div>
 				</div>
 			</div>
