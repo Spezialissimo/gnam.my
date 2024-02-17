4744935bb9d7bfa7dfcc3d53c5ea05b53abb7f01
Spezialissimo <davide.speziali@proton.me>
2024-02-17 19:41:22 +0100

    Navbar is now usable eith keyboard

--- core/template/footer.php
+++ core/template/footer.php
@@ -2,35 +2,30 @@
 			</div>
 			<nav class="m-0">
 				<div class="w-100 d-flex" id="navbarContentDiv">
-					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="home.php" class="btn bg-primary d-flex p-sm-3 p-2 border-0">
-							<img alt="Pagina Home" src="assets/home.png" />
+					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center" >
+						<a href="home.php" class="d-flex p-2 border-0" role="button" tabindex="1">
+							<img class="bg-primary" alt="Pagina Home" src="assets/home.png" /><span class="ps-4 nav-text text-button fw-semibold color-accent">Home</span>
 						</a>
-						<a href="home.php" class="ps-2 nav-text text-button fw-semibold color-accent">Home</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="search.php" class="btn bg-primary d-flex p-sm-3 p-2 border-0">
-							<img alt="Pagina Cerca" src="assets/search.png" />
+						<a href="search.php" class="d-flex p-2 border-0" role="button" tabindex="1">
+							<img class="bg-primary" alt="Pagina Cerca" src="assets/search.png" /><span class="ps-4 nav-text text-button fw-semibold color-accent">Cerca</span>
 						</a>
-						<a href="search.php" class="ps-2 nav-text text-button fw-semibold color-accent">Cerca</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="publish.php" class="btn bg-primary d-flex p-sm-3 p-2 border-0">
-							<img alt="Pagina Pubblica" src="assets/publish.png" />
+						<a href="publish.php" class="d-flex p-2 border-0" role="button" tabindex="1">
+							<img class="bg-primary" alt="Pagina Pubblica" src="assets/publish.png" /><span class="ps-4 nav-text text-button fw-semibold color-accent">Pubblica</span>
 						</a>
-						<a href="publish.php" class="ps-2 nav-text text-button fw-semibold color-accent">Pubblica</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="notifications.php" class="btn bg-primary d-flex p-sm-3 p-2 border-0">
-							<img alt="Pagina Notifiche" src="assets/notifications.png" id="notificationsNavbarImg" />
+						<a href="notifications.php" class="d-flex p-2 border-0" role="button" tabindex="1">
+							<img class="bg-primary" alt="Pagina Notifiche" src="assets/notifications.png" id="notificationsNavbarImg" /><span class="ps-4 nav-text text-button fw-semibold color-accent">Notifiche</span>
 						</a>
-						<a href="notifications.php" class="ps-2 nav-text text-button fw-semibold color-accent">Notifiche</a>
 					</div>
 					<div class="btn-bounce d-flex flex-row align-items-center justify-content-center">
-						<a role="button" href="profile.php" class="btn bg-primary d-flex p-sm-3 p-2 border-0">
-							<img alt="Pagina Profilo" src="assets/profile.png" />
+						<a href="profile.php" class="d-flex p-2 border-0" role="button" tabindex="1">
+							<img class="bg-primary" alt="Pagina Profilo" src="assets/profile.png" /><span class="ps-4 nav-text text-button fw-semibold color-accent">Profilo</a>
 						</a>
-						<a href="profile.php" class="ps-2 nav-text text-button fw-semibold color-accent">Profilo</a>
 					</div>
 				</div>
 			</nav>
