361e0c1b534a2dd9424a01dafd9f44d930f408f0
Mauro Pellonara <mauro.pellonara@gmail.com>
2024-02-23 20:53:15 +0100

    add accessibility to results in search

--- core/template/search.php
+++ core/template/search.php
@@ -137,7 +137,7 @@
             let rowDiv = $('<div class="row my-3">');
 
             for (let o in currentResult) {
-                let img = $(`<img class="img-grid px-2 col-4 btn-bounce cursor-pointer" id="searchResultGnam-${currentResult[o].id}" alt="Copertina gnam" src="assets/gnams_thumbnails/${currentResult[o].id}.jpg" />`);
+                let img = $(`<img class="img-grid px-2 col-4 btn-bounce cursor-pointer" id="searchResultGnam-${currentResult[o].id}" alt="Copertina gnam di ${currentResult[o].name}" src="assets/gnams_thumbnails/${currentResult[o].id}.jpg" aria-label="Copertina dello gnam ${rowDiv.children.length + (3 - gnamPerRow) + 1} di ${currentResult[o].name}"  tabindex="3" />`);
                 rowDiv.append(img);
                 gnamPerRow--;
 
